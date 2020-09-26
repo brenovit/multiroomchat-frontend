9875890e9db56b57000483c162ffd4477026a5aa
Breno Nunes <breno.arantes97@gmail.com>
2020-09-26 19:30:38 -0300

    Upgrade vue to version 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Vue from "vue";
+import Vue, { createApp } from "vue";
 import App from "./App.vue";
 import $ from "jquery";
 import LoadScript from "vue-plugin-load-script";
@@ -9,9 +9,6 @@ window.jQuery = window.$ = $;
 require("bootstrap/dist/css/bootstrap.min.css");
 require("bootstrap/dist/js/bootstrap.min.js");
 
-Vue.config.productionTip = false;
 Vue.loadScript("/js/swipe.min.js");
 
-new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+createApp(App).mount("#app");
