a88652ae0f0c9fd59e89300fc3b11b1483623ea8
Breno Nunes <breno.arantes97@gmail.com>
2020-09-29 22:54:39 -0300

    Break chat component
    - Input message
    - Top Bar Chat

---
+++ src/components/chat/InputMessageComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="col-md-12">
+      <div class="bottom">
+        <form class="position-relative w-100">
+          <textarea
+            class="form-control"
+            placeholder="Start typing for reply..."
+            rows="1"
+          ></textarea>
+          <button class="btn emoticons">
+            <i class="material-icons">insert_emoticon</i>
+          </button>
+          <button type="submit" class="btn send">
+            <i class="material-icons">send</i>
+          </button>
+        </form>
+        <label>
+          <input type="file" />
+          <span class="btn attach d-sm-block d-none"
+            ><i class="material-icons">attach_file</i></span
+          >
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "InputComponent",
+});
+</script>
--- src/components/chat/TypingComponent.vue
+++ src/components/chat/TypingComponent.vue
@@ -1,21 +1,19 @@
 <template>
-  <div>
-    <img
-      class="avatar-md"
-      src="img/avatars/avatar-female-5.jpg"
-      data-toggle="tooltip"
-      data-placement="top"
-      title="Keith"
-      alt="avatar"
-    />
-    <div class="text-main">
-      <div class="text-group">
-        <div class="text typing">
-          <div class="wave">
-            <span class="dot"></span>
-            <span class="dot"></span>
-            <span class="dot"></span>
-          </div>
+  <img
+    class="avatar-md"
+    src="img/avatars/avatar-female-5.jpg"
+    data-toggle="tooltip"
+    data-placement="top"
+    title="Keith"
+    alt="avatar"
+  />
+  <div class="text-main">
+    <div class="text-group">
+      <div class="text typing">
+        <div class="wave">
+          <span class="dot"></span>
+          <span class="dot"></span>
+          <span class="dot"></span>
         </div>
       </div>
     </div>
